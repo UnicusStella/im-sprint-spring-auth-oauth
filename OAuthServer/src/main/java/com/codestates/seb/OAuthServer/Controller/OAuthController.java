07f4a59874e91f37057723aa003053b408c68c39
UnicusStella <unicusstella@gmail.com>
2021-09-08 11:29:41 +0900

    Controller Complte

--- OAuthServer/src/main/java/com/codestates/seb/OAuthServer/Controller/OAuthController.java
+++ OAuthServer/src/main/java/com/codestates/seb/OAuthServer/Controller/OAuthController.java
@@ -52,7 +52,7 @@ public class OAuthController {
     public ResponseEntity<?> GetImageData(@RequestHeader Map<String, String> header){
         // 헤더에 authorization 값을 확인하여 정보가 있다면 이미지 경로 데이터를 응답합니다.
         // TODO :
-        if(){ // 헤더에 내용을 검증하는 내용을 조건을 작성합니다.
+        if(header != null){ // 헤더에 내용을 검증하는 내용을 조건을 작성합니다.
             return ResponseEntity.ok()
                     .body(new HashMap<String, ArrayList<Resources>>(){{put("images", ResourcesData.getInstance().getResourcesList());}});
         }else{
--- OAuthServer/src/main/resources/import.sql
+++ OAuthServer/src/main/resources/import.sql
@@ -1,1 +1,1 @@
-INSERT INTO oauth_code (id, client_id, client_secret) VALUES(0, "client_id", "client_secret");
+INSERT INTO oauth_code (id, client_id, client_secret) VALUES(0, "72c1ff94dc81db95b984", "26b29bc9b78d5fe4980b40f95f72c1331af5440d");
